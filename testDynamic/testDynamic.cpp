f6de889399ec0390f3bf50b2abd82d0998dc814e
ThienPham <unhate.everything@gmail.com>
2021-06-02 17:10:01 +0500

    Add project files.

---
+++ testDynamic/testDynamic.cpp
@@ -0,0 +1,13 @@
+// testDynamic.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include "Header.h"
+
+int main()
+{
+    std::cout << "My group is "<< MyGroup(561220) << "\n";
+    std::cout << "My name is " << MyName("Pham Xuan Thien") << "\n";
+    system("pause");
+    return 0;
+}
+
